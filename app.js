004e3d5f475e7412b40987c8c4892d976c75a801
DesRails <frase101@umn.edu>
2017-02-23 21:46:46 -0600

    X on click

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  $('.square').click(function(event) {
+  $(".square").click(function(event) {
     var clickedSquare = event.target.id;
-    alert("hi you clicked on "+clickedSquare);
+    $("#"+clickedSquare).html("X");
   });
 });
