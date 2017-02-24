88abb2e4ffa0f87bcca44a2af947de2ca556258c
DesRails <frase101@umn.edu>
2017-02-23 22:07:46 -0600

    prevent user from clicking on a filled square

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ $(document).ready(function(){
   $(".square").click(function(event) {
     var clickedSquareName = event.target.id;
     var clickedSquare = $("#"+clickedSquareName);
+    var clickedSquareValue = clickedSquare.html();
+
+    if (clickedSquareValue == "X" || clickedSquareValue == "O") {
+      alert("you idiot");
+      return;
+    }
 
     if (lastValueEntered == "X") {
       clickedSquare.html("O");
